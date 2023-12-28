61bebb0a0d4c011d36f97e5ad511de435f93ab1c
Said Alisic <saidalisic@Saids-MacBook-Pro.local>
2023-12-28 22:52:52 +0100

    feat: 🎸 Create GetCalenderEventResponse interface and dto

---
+++ src/Common/Dto/GetCalendarEventResponseDto.cs
@@ -0,0 +1,20 @@
+﻿using API.Common.Interfaces;
+using API.Models;
+
+namespace API.Common.Dto;
+
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+public class GetCalendarEventResponseDto : IGetCalendarEventResponseDto
+{
+    public Guid Id { get; set; }
+
+    public DateTime DateAndTime { get; set; }
+
+    public string Title { get; set; }
+
+    public string Location { get; set; }
+
+    public string Status { get; set; }
+
+    public List<Person>? EventParticipants { get; set; }
+};
---
+++ src/Common/Interfaces/IGetCalendarEventResponseDto.cs
@@ -0,0 +1,15 @@
+﻿using API.Models;
+
+namespace API.Common.Interfaces;
+
+public interface IGetCalendarEventResponseDto
+{
+    public Guid Id { get; set; }
+    public DateTime DateAndTime { get; set; }
+    public string Title { get; }
+
+    public string Location { get; set; }
+    public string Status { get; set; }
+
+    List<Person>? EventParticipants { get; set; }
+}
